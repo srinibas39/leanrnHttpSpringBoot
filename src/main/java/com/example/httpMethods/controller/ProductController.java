63dac8448f88200144528e4975186a7ecaa8cfa7
srinibas khuntia <srinibas.k@pillir.io>
2025-03-18 18:44:33 +0530

    logger

--- src/main/java/com/example/httpMethods/controller/ProductController.java
+++ src/main/java/com/example/httpMethods/controller/ProductController.java
@@ -1,12 +1,17 @@
 package com.example.httpMethods.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
+    Logger logger = LoggerFactory.getLogger(ProductController.class);
 
     @GetMapping ("/product")
     public String getProduct(@RequestParam("name") String name , @RequestParam("id") int id){
@@ -20,9 +25,13 @@ public class ProductController {
 
     @GetMapping("/product/{id}/{name}/{color}")
     public String checkProduct(@PathVariable("id") int productId,@PathVariable("name") String name , @PathVariable("color") String color){
-        System.out.println("name "+name);
-        System.out.println("id "+productId);
-        System.out.println("color "+color);
+//        System.out.println("name "+name);
+//        System.out.println("id "+productId);
+//        System.out.println("color "+color);
+
+        logger.error("name "+name);
+        logger.warn("color "+color);
+        logger.info("color "+color);
         return "Product has path variable";
     }
 }
